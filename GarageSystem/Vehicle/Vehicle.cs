c740b0ffa05d78530569e05cab317257ea7e1d80
Gommash <a.lekedal@gmail.com>
2017-05-11 14:11:01 +0200

    Changed a little

--- GarageSystem/Vehicle/Vehicle.cs
+++ GarageSystem/Vehicle/Vehicle.cs
@@ -4,24 +4,32 @@ namespace GarageSystem
 {
     class Vehicle
     {
+        #region Properties
         public string RegNumber { set; get; } //This value should always be unique
         public decimal ParkingPrice { set; get; } //Price for parking
         public DateTime ParkingDate { set; get; } //Date variable for Parking
-       
+        #endregion
+
+        #region Constructor
         public Vehicle()//Constructor
         {
             ParkingDate = DateTime.Now; //Setting the ParkingDate to the date on the system
         }
+        #endregion
+
+        #region Methods
         public string GetObjectType() //Returns the object type so that we can get what type the Vehicle is
         {
             return this.GetType().ToString();
         }
+        
         public bool Equals(Vehicle other) //Equals - check if an instance of Vehicle is the same as this instance
         {
             if (other == null) return false;
             if (this.RegNumber == other.RegNumber) return true;
             else return false;
         }
+        
         public override bool Equals(object obj) //Equals - Check if an object is same type as Vehicle
         {
             if (obj == null) return false;
@@ -30,5 +38,6 @@ namespace GarageSystem
             if (vehicleObject == null) return false;
             else return Equals(vehicleObject);
         }
+        #endregion
     }
 }
