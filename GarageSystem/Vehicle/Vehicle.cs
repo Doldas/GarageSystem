f1a89adff2d62c0b15f076796cf1ec6b918ac2db
Gommash <a.lekedal@gmail.com>
2017-05-11 11:25:57 +0200

    Iquatable is added

--- GarageSystem/Vehicle/Vehicle.cs
+++ GarageSystem/Vehicle/Vehicle.cs
@@ -6,16 +6,32 @@ using System.Threading.Tasks;
 
 namespace GarageSystem
 {
-    class Vehicle : IEquatable<Vehicle>,IComparable<Vehicle>
+    class Vehicle : IEquatable<Vehicle>
     {
+        public string regNumber { set; get; }
+
+        //Check if a vehicle is the same as another vehicle
         bool IEquatable<Vehicle>.Equals(Vehicle other)
         {
-            throw new NotImplementedException();
+            return this.regNumber == other.regNumber;
         }
-
-        int IComparable<Vehicle>.CompareTo(Vehicle other)
+        override bool IEquatable<Vehicle>.Equals(object other)
         {
-            throw new NotImplementedException();
+            /*
+            Vehicle vehicle = other as Vehicle; 
+             * In theory a class Car should be true here because of inheritance, but not sure until later testing
+             * If the code above works we can remove all other casts
+             */
+            Car car = other as Car;
+            Bus bus = other as Buss;
+            Motorcycle motorcycle = other as Motorcycle;
+           //If these variables are empty, return false because that means it isn't a vehicle
+            if(car==null && bus==null && motorcycle==null)
+            {
+                return false;
+            }
+            //Else return true
+            return true;
         }
     }
 }
